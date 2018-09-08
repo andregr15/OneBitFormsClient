044598a1b2e1c3aeadb54095f60fd7c993163d86
André Rodrigues <andre.goncalves.rodrigues@gmail.com>
2018-09-07 23:12:13 -0300

    Implementing active component

--- src/app/forms/shared/active/active.component.ts
+++ src/app/forms/shared/active/active.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormService } from '../../../shared/form.service';
+import { MzToastService } from 'ngx-materialize';
+import { Form } from '../../../shared/form.model';
 
 @Component({
   selector: 'app-active',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActiveComponent implements OnInit {
 
-  constructor() { }
+  @Input() form: Form;
+
+  constructor(private service: FormService, private toastService: MzToastService) { }
 
   ngOnInit() {
+  }
+
+  toggleEnable() {
+    this.service.updateForm(this.form.id, this.form).subscribe(
+      res => {
+        this.toastService.show(`Form ${this.form.enable ? 'enable' : 'disable'}`, 8000, 'green');
+      },
+      error => {
+        this.toastService.show(`Error in ${this.form.enable ? 'enable' : 'disable'} Form`, 8000, 'red');
+      }
+    )
   }
 
 }
